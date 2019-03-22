ff49d738075c42fb5d812a1c896d2464146a343d
liushuang <mrxyliushuang@qq.com>
2019-03-22 21:22:56 +0800

    验证登陆

--- src/main/java/com/ocean/controller/login/LoginController.java
+++ src/main/java/com/ocean/controller/login/LoginController.java
@@ -42,7 +42,7 @@ public class LoginController {
      * @param username 用户名
      * @param password 密码
      */
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
     public ResultMap login(String username, String password) {
         // 从SecurityUtils里边创建一个 subject
         Subject subject = SecurityUtils.getSubject();
