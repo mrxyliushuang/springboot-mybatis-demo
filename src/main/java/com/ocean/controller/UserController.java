1d685fabf26d0a902f6b8c00532efa5d6fe2cf0d
liushuang <mrxyliushuang@qq.com>
2019-03-24 23:30:43 +0800

    添加登录者我的信息的返回

--- src/main/java/com/ocean/controller/UserController.java
+++ src/main/java/com/ocean/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.ocean.controller;
 
+import com.ocean.pojo.TUser;
 import com.ocean.service.IUserService;
+import com.ocean.utils.Json;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +14,22 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping(value = {"/user"})
+@RequestMapping(value = {"/commn"})
 public class UserController {
+//
+//    @Autowired
+//    private IUserService userService;
+//
+//    @RequestMapping(value = {"/findAll"},produces = {"application/json;charset=UTF-8"},method = RequestMethod.GET)
+//    public List getAllUsers(){
+//       List list =  userService.findAllUser();
+//       return list;
+//    }
 
-    @Autowired
-    private IUserService userService;
-
-    @RequestMapping(value = {"/findAll"},produces = {"application/json;charset=UTF-8"},method = RequestMethod.GET)
-    public List getAllUsers(){
-       List list =  userService.findAllUser();
-       return list;
+    @RequestMapping(value = "/getMyInfo", method = RequestMethod.GET)
+    public Json getMyInfo() {
+        TUser tUser = (TUser) SecurityUtils.getSubject().getPrincipal();
+        System.out.println("username:" + tUser.toString());
+        return Json.succ("current user info", tUser);
     }
 }
